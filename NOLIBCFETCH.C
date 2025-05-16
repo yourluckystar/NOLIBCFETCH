f0e88c86b3cf5601c762914a80f4b4aa69e132b6
Your <184955262+yourluckystar@users.noreply.github.com>
2025-05-17 08:23:10 +0900

    itoa and err

--- NOLIBCFETCH.C
+++ NOLIBCFETCH.C
@@ -2,6 +2,49 @@
  * NOLIBCFETCH(1) - nolibc fetch in asm+c .3
  */
 
+#define SYS_exit 60
+#define SYS_uname 63
+#define SYS_sysinfo 99
+
+static void err(const char *fmt, ...);
+static void itoa(int num, char *buf);
+static unsigned long strlen(const char *s);
+static void print_sysinfo(void);
+
+void
+err(const char *fmt, ...)
+{
+        /* TODO */
+}
+
+/* not a fully implemented itoa because i only need base 10
+ * and as far as i know i dont need to check for negative numbers
+ * (unless i do in which case yell at me ty) */
+void
+itoa(int num, char *buf)
+{
+        int i = 0;
+        int j, k;
+
+        if (num == 0) {
+                buf[0] = '0';
+                buf[1] = '\0';
+                return;
+        }
+
+        while (num > 0) {
+                buf[i++] = num % 10 + '0';
+                num /= 10;
+        }
+        buf[i] = '\0';
+
+        for (j = 0, k = i - 1; j < k; j++, k--) {
+                char tmp = buf[j];
+                buf[j] = buf[k];
+                buf[k] = tmp;
+        }
+}
+
 int
 main(int argc, char *argv[])
 {
